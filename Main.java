9feaf8af7193813e32a99685bb29a4f0c326ddaa
milese3 <miles.elvidge@ntlworld.com>
2014-03-02 21:36:25 +0000

    Added Code

---
+++ Main.java
@@ -0,0 +1,50 @@
+package me.JoelyMo101.holeinthewall;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	
+	public void onDisable() {
+		reloadConfig();
+		saveConfig();
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player))
+		{
+			sender.sendMessage(ChatColor.RED + "The console cannot use Hole In The Wall!");
+		}
+		Player p = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("hitw"))
+		{
+			if (args.length < 1 || args[0].equalsIgnoreCase("help"))
+			{
+				this.helpScreen(p);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public void helpScreen(Player p)
+	{
+		p.sendMessage(ChatColor.GREEN + "" + ChatColor.STRIKETHROUGH + "*******" + ChatColor.GREEN + " Hole In The Wall - Help " + ChatColor.GREEN + "" + ChatColor.STRIKETHROUGH + "*******");
+		p.sendMessage(ChatColor.DARK_GREEN + "/hitw help" + ChatColor.GREEN + " - Displays this help screen.");
+		p.sendMessage(ChatColor.DARK_GREEN + "/hitw join" + ChatColor.GREEN + " - Join the queue.");
+		p.sendMessage(ChatColor.DARK_GREEN + "/hitw leave" + ChatColor.GREEN + " - Leave the game or queue you are in.");
+		p.sendMessage(ChatColor.DARK_GREEN + "/hitw " + ChatColor.GREEN + " - Leave the game or queue you are in.");		
+	}
+}
